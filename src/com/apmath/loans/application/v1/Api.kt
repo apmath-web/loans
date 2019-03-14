c1fce4df388c0e017fc6681a6ac0a998f348140c
BarracudaPff <mrm.kikll@gmail.com>
2019-03-14 11:57:47 +0300

    add info test

--- src/com/apmath/loans/application/v1/Api.kt
+++ src/com/apmath/loans/application/v1/Api.kt
@@ -1,7 +1,20 @@
 package com.apmath.loans.application.v1
 
+import com.apmath.loans.application.v1.actions.v1Info
+import io.ktor.application.call
 import io.ktor.routing.Routing
+import io.ktor.routing.get
+import io.ktor.routing.route
 
 internal fun Routing.v1() {
+    v1Info()
+}
 
+private fun Routing.v1Info() {
+
+    route("v1") {
+        get("info") {
+            call.v1Info()
+        }
+    }
 }
---
+++ src/com/apmath/loans/application/v1/actions/Info.kt
@@ -0,0 +1,9 @@
+package com.apmath.loans.application.v1.actions
+
+import com.apmath.loans.application.v1.models.Info
+import io.ktor.application.ApplicationCall
+import io.ktor.response.respond
+
+suspend fun ApplicationCall.v1Info() {
+    respond(Info( "0.1"))
+}
