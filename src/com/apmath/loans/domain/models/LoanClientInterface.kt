838ee0077146431f48280d1345e0ef227cbcd7ca
BarracudaPff <mrm.kikll@gmail.com>
2019-03-23 05:46:15 +0300

    add some methods

--- src/com/apmath/loans/domain/models/LoanClientInterface.kt
+++ src/com/apmath/loans/domain/models/LoanClientInterface.kt
@@ -1,10 +1,16 @@
 package com.apmath.loans.domain.models
 
+import com.apmath.loans.application.v1.models.PaymentInterface as RequestPaymentInterface
+import com.apmath.loans.domain.models.connections.response.PaymentInterface as ResponsePaymentInterface
 import com.apmath.loans.domain.models.data.Money
+import com.apmath.loans.domain.models.data.Type
 
 interface LoanClientInterface {
     val amount: Money
     val term: Int
     val interest: Money
     val date: String
+
+    fun getPayments(type: Type?): List<ResponsePaymentInterface>
+    fun writeOf(paymentRequest: RequestPaymentInterface)
 }
