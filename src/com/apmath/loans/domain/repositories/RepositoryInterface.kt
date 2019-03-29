8d7ae868e8021041f91a4d65dd603d9ed63ff11d
Neirwin <nightablackstone@gmail.com>
2019-03-29 14:49:55 +0300

    Added repository

--- src/com/apmath/loans/domain/repositories/RepositoryInterface.kt
+++ src/com/apmath/loans/domain/repositories/RepositoryInterface.kt
@@ -4,6 +4,6 @@ import com.apmath.loans.domain.models.LoanEmployeeInterface
 
 interface RepositoryInterface {
     fun get(id: Int): LoanEmployeeInterface
-    fun store(credit: LoanEmployeeInterface)
-    fun remove(credit: LoanEmployeeInterface)
+    fun store(loan: LoanEmployeeInterface)
+    fun remove(loan: LoanEmployeeInterface)
 }
