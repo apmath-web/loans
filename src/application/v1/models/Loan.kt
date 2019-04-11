6b7e94dd75f9745901fe3e157a758cd0ccbaedba
BarracudaPff <mrm.kikll@gmail.com>
2019-04-11 10:34:48 +0300

    add actions

--- src/application/v1/models/Loan.kt
+++ src/application/v1/models/Loan.kt
@@ -1,12 +1,24 @@
 package com.apmath.loans.application.v1.models
 
+import com.apmath.loans.domain.models.Loan as LoanDomain
 import com.apmath.loans.domain.models.data.Currency
 import com.apmath.loans.domain.models.data.Money
 
 class Loan {
+    var clientId: Int? = null
     var applicationId: Int? = null
     var amount: Money? = null
     var currency: Currency? = null
     var term: Int? = null
     var date: String? = null
 }
+
+//TODO: replace loanClient from dump
+fun Loan.toLoanClient() = LoanDomain(
+    clientId = clientId!!,
+    applicationId = applicationId!!,
+    amount = amount!!,
+    currency = currency!!,
+    term = term!!,
+    date = date!!
+)
