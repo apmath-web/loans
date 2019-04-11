779b781cfced648812ea9ae991bf1d3f564ce14e
BarracudaPff <mrm.kikll@gmail.com>
2019-04-11 10:36:13 +0300

    append api

--- src/application/v1/Api.kt
+++ src/application/v1/Api.kt
@@ -1,9 +1,11 @@
 package com.apmath.loans.application.v1
 
+import com.apmath.loans.application.v1.actions.v1Create
 import com.apmath.loans.application.v1.actions.v1Info
 import io.ktor.application.call
 import io.ktor.routing.Routing
 import io.ktor.routing.get
+import io.ktor.routing.post
 import io.ktor.routing.route
 
 internal fun Routing.v1() {
@@ -17,6 +19,9 @@ private fun Routing.v1Info() {
     route("v1") {
         get("info") {
             call.v1Info()
+        }
+        post {
+            call.v1Create()
         }
     }
 }
