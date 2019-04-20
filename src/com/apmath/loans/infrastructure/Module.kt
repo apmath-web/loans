163ef8bf63b8dc8d105c6caab03391389bc7efb8
BarracudaPff <mrm.kikll@gmail.com>
2019-04-20 06:52:53 +0300

    append module

--- src/com/apmath/loans/infrastructure/Module.kt
+++ src/com/apmath/loans/infrastructure/Module.kt
@@ -1,10 +1,22 @@
 package com.apmath.loans.com.apmath.loans.infrastructure
 
+import com.apmath.loans.domain.fetchers.*
+import com.apmath.loans.domain.repositories.*
+import com.apmath.loans.domain.services.*
+import com.apmath.loans.infrastructure.fetchers.*
 import org.koin.dsl.module
+import org.koin.experimental.builder.singleBy
 
 /**
  * Module in which services connect to their interfaces
  */
 val loans = module {
-    //TODO: Add dependencies here
+    //services
+    singleBy<LoanServiceInterface, LoanService>()
+    //fetchers
+    singleBy<ApplicationsFetcherInterface, ApplicationsFetcher>()
+    singleBy<CalculationsFetcherInterface, CalculationsFetcher>()
+    singleBy<ClientsFetcherInterface, ClientsFetcher>()
+    //repositories
+    singleBy<RepositoryInterface, Repository>()
 }
