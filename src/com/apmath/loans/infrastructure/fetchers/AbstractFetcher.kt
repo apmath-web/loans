3696647978735f7676c5f8973755df2ebc4f73e9
BarracudaPff <mrm.kikll@gmail.com>
2019-04-20 06:50:31 +0300

    add abstract fetcher

--- src/com/apmath/loans/infrastructure/fetchers/AbstractFetcher.kt
+++ src/com/apmath/loans/infrastructure/fetchers/AbstractFetcher.kt
@@ -1,12 +1,16 @@
 package com.apmath.loans.infrastructure.fetchers
 
 import io.ktor.client.HttpClient
+import io.ktor.client.call.call
 import io.ktor.client.engine.apache.Apache
 import io.ktor.client.features.json.GsonSerializer
 import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.request.get
-import io.ktor.client.request.post
-import io.ktor.client.request.put
+import io.ktor.client.request.request
+import io.ktor.client.request.url
+import io.ktor.http.ContentType
+import io.ktor.http.HttpMethod
+import io.ktor.http.contentType
+import io.ktor.http.isSuccess
 
 open class AbstractFetcher(
     var host: Host,
@@ -14,30 +18,50 @@ open class AbstractFetcher(
 ) {
     init {
         if (testing)
-            host = Host.JSON_PLACE_HOLDER
+            host = Host.TESTING_PLACE_HOLDER
     }
 
     suspend inline fun <reified T> get(path: String): T {
-        return AbstractFetcher.client.get(
-            host = host.value,
-            path = path
-        )
+        return AbstractFetcher.client.request {
+            method = HttpMethod.Get
+            url(
+                host = host.value,
+                path = path
+            )
+        }
     }
 
-    suspend inline fun <reified T> post(path: String, body: Any): T {
-        return AbstractFetcher.client.post(
-            host = host.value,
-            path = path,
-            body = body
-        )
+    suspend inline fun <reified T> post(path: String, bodyReq: Any): T {
+        return AbstractFetcher.client.request {
+            method = HttpMethod.Post
+            url(
+                host = host.value,
+                path = path
+            )
+            body = bodyReq
+            contentType(ContentType.Application.Json)
+        }
     }
 
-    suspend inline fun <reified T> put(path: String, body: Any): T {
-        return AbstractFetcher.client.put(
-            host = host.value,
-            path = path,
-            body = body
-        )
+    suspend inline fun <reified T> put(path: String, bodyReq: Any): T {
+        return AbstractFetcher.client.request {
+            method = HttpMethod.Put
+            url(
+                host = host.value,
+                path = path
+            )
+            body = bodyReq
+            contentType(ContentType.Application.Json)
+        }
+    }
+
+    suspend inline fun isSuccess(path: String): Boolean {
+        return AbstractFetcher.client.call {
+            url(
+                host = host.value,
+                path = path
+            )
+        }.response.status.isSuccess()
     }
 
     companion object {
