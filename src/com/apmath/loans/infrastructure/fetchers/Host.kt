bf2f0f61db1b115d1a68f641d3f231bf1e319f22
BarracudaPff <mrm.kikll@gmail.com>
2019-04-26 21:42:19 +0300

    now hosts from config

--- src/com/apmath/loans/infrastructure/fetchers/Host.kt
+++ src/com/apmath/loans/infrastructure/fetchers/Host.kt
@@ -1,8 +1,19 @@
 package com.apmath.loans.infrastructure.fetchers
 
+import com.typesafe.config.ConfigFactory
+import io.ktor.config.HoconApplicationConfig
+
 enum class Host(val value: String) {
-    CLIENTS("localhost:8080"),
-    APPLICATIONS("localhost:8081"),
-    CALCULATIONS("localhost:8082"),
-    TESTING_PLACE_HOLDER("demo5033957.mockable.io");
+    CLIENTS(host("clients")),
+    APPLICATIONS(host("applications")),
+    CALCULATIONS(host("calculations")),
+}
+
+private fun host(name: String): String {
+    val config = HoconApplicationConfig(ConfigFactory.load())
+
+    return if (config.property("$name.testing").getString() == "true")
+        config.property("$name.domain").getString()
+    else
+        config.property("mock.domain").getString()
 }
