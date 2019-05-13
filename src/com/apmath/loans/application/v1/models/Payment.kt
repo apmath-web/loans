3c8902335ef4e40ae7f71ba8d25056b9c5525527
meleorina <meleorina@gmail.com>
2019-05-13 19:49:43 +0300

    add clientId & to payment domain

--- src/com/apmath/loans/application/v1/models/Payment.kt
+++ src/com/apmath/loans/application/v1/models/Payment.kt
@@ -2,9 +2,18 @@ package com.apmath.loans.application.v1.models
 
 import com.apmath.loans.domain.data.Currency
 import com.apmath.loans.domain.data.Money
+import com.apmath.loans.domain.models.payments.Payment as PaymentDomain
 
 class Payment {
     var payment: Money? = null
     var currency: Currency? = null
     var date: String? = null
+    var clientId: Int? = null
 }
+
+fun Payment.toPayment() = PaymentDomain (
+    payment!!,
+    currency!!,
+    date!!,
+    clientId!!
+)
