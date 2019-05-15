f73a5052805e6d6f5cdd332d2aea0312630a8ad2
meleorina <meleorina@gmail.com>
2019-05-15 12:13:56 +0300

    change date type

--- src/com/apmath/loans/domain/models/payments/Payment.kt
+++ src/com/apmath/loans/domain/models/payments/Payment.kt
@@ -2,10 +2,11 @@ package com.apmath.loans.domain.models.payments
 
 import com.apmath.loans.domain.data.Currency
 import com.apmath.loans.domain.data.Money
+import java.time.LocalDate
 
 data class Payment(
     override val payment: Money,
     override val currency: Currency,
-    override val date: String,
+    override val date: LocalDate,
     override val clientId: Int
 ) : PaymentInterface
--- src/com/apmath/loans/domain/models/payments/PaymentInterface.kt
+++ src/com/apmath/loans/domain/models/payments/PaymentInterface.kt
@@ -2,10 +2,11 @@ package com.apmath.loans.domain.models.payments
 
 import com.apmath.loans.domain.data.Currency
 import com.apmath.loans.domain.data.Money
+import java.time.LocalDate
 
 interface PaymentInterface {
     val payment: Money
     val currency: Currency
-    val date: String
+    val date: LocalDate
     val clientId: Int
 }
