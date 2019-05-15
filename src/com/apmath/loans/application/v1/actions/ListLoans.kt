84c0a3de8c60625946057700e009462a1d4f4247
BarracudaPff <mrm.kikll@gmail.com>
2019-05-15 10:37:56 +0300

    upd action

--- src/com/apmath/loans/application/v1/actions/ListLoans.kt
+++ src/com/apmath/loans/application/v1/actions/ListLoans.kt
@@ -1,40 +1,54 @@
 package com.apmath.loans.application.v1.actions
 
-import com.apmath.loans.application.v1.models.Mixed
-import com.apmath.loans.application.v1.models.toMixedId
+import com.apmath.loans.application.v1.models.LoansListRequest
 import com.apmath.loans.application.v1.respondError
 import com.apmath.loans.application.v1.validators.MixedBuilder
 import com.apmath.loans.domain.services.LoanServiceInterface
 import com.apmath.validation.simple.NullableValidator
+import com.apmath.validation.simple.RequiredValidator
 import io.ktor.application.ApplicationCall
 import io.ktor.response.respond
 
 suspend fun ApplicationCall.v1ListLoans(loanService: LoanServiceInterface) {
-    val mixed = Mixed(
+    val request = LoansListRequest(
         getUserId(request),
-        request.headers["service"],
-        parameters["client"]
+        isService(request),
+        getClientAttributeId(this)
     )
 
     val validator = MixedBuilder()
         .prepend("clientIdHeader", NullableValidator())
-        .prepend("serviceIdHeader", NullableValidator())
+        .prepend("isService", RequiredValidator())
         .prepend("clientId", NullableValidator())
         .build()
 
-    if (!validator.validate(mixed)) {
+    if (!validator.validate(request)) {
         respond(validator.messages)
         return
     }
 
-    val mixedId = mixed.toMixedId()
     val loans =
         try {
-            loanService.get(mixedId)
+            loanService.get(request.isService, request.clientIdHeader, request.clientId)
         } catch (e: Exception) {
             respondError(e)
             return
         }
 
     respond(mapOf("loans" to loans))
+}
+
+private fun getClientAttributeId(call: ApplicationCall): Int? {
+
+    val userHeaderKey = "client"
+
+    if (call.parameters.contains(userHeaderKey)) {
+        try {
+            return call.parameters[userHeaderKey]?.toInt()
+        } catch (e: NumberFormatException) {
+
+        }
+    }
+
+    return null
 }
