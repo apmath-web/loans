013b179157d6aa86e17f07045bfbfe1cb9c6d22a
BarracudaPff <mrm.kikll@gmail.com>
2019-05-15 10:36:41 +0300

    rewrite validator builder

--- src/com/apmath/loans/application/v1/validators/MixedBuilder.kt
+++ src/com/apmath/loans/application/v1/validators/MixedBuilder.kt
@@ -2,13 +2,10 @@ package com.apmath.loans.application.v1.validators
 
 import com.apmath.validation.builders.ObjectValidatorBuilder
 import com.apmath.validation.simple.ComparableValidator
-import com.apmath.validation.simple.RegexValidator
 
 class MixedBuilder : ObjectValidatorBuilder() {
     init {
-        val onlyNumber = "\\d*[1-9]\\d*"
-
         append("clientIdHeader", ComparableValidator(min = 1))
-        append("clientId", RegexValidator(onlyNumber))
+        append("clientId", ComparableValidator(min = 1))
     }
 }
