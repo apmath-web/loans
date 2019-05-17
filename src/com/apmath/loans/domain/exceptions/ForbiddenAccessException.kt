0eeae1573bb3ec1640d761e384629892b6091dea
BarracudaPff <mrm.kikll@gmail.com>
2019-05-17 13:16:10 +0300

    rewrite domain exceptions

--- src/com/apmath/loans/domain/exceptions/ForbiddenAccessException.kt
+++ src/com/apmath/loans/domain/exceptions/ForbiddenAccessException.kt
@@ -1,6 +1,3 @@
 package com.apmath.loans.domain.exceptions
 
-import io.ktor.http.HttpStatusCode
-
-class ForbiddenAccessException :
-    ApiException("Permission denied", HttpStatusCode.Forbidden)
+class ForbiddenAccessException : Exception()
--- src/com/apmath/loans/domain/exceptions/WrongClientId.kt
+++ src/com/apmath/loans/domain/exceptions/WrongClientId.kt
@@ -1,5 +1,3 @@
 package com.apmath.loans.domain.exceptions
 
-import io.ktor.http.HttpStatusCode
-
-class WrongClientId : ApiException("Wrong client", HttpStatusCode.BadRequest)
+class WrongClientId : Exception()
