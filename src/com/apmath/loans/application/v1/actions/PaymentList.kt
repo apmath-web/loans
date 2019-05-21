9f5f4bfa7d7cbe77994b5fd6bda9b56fa4f002a4
Neirwin <nightablackstone@gmail.com>
2019-05-21 18:21:51 +0300

    rewrote action

--- src/com/apmath/loans/application/v1/actions/PaymentList.kt
+++ src/com/apmath/loans/application/v1/actions/PaymentList.kt
@@ -1,43 +1,43 @@
 package com.apmath.loans.application.v1.actions
 
-import com.apmath.loans.application.v1.models.MixedLoan
-import com.apmath.loans.application.v1.models.toMixedLoanId
-import com.apmath.loans.application.v1.respondError
-import com.apmath.loans.application.v1.validators.MixedLoanBuilder
+import com.apmath.loans.application.v1.exceptions.NotFoundException
+import com.apmath.loans.domain.exceptions.LoanNotFoundException
 import com.apmath.loans.domain.services.PaymentServiceInterface
-import com.apmath.validation.simple.NullableValidator
-import com.apmath.validation.simple.RequiredValidator
 import com.apmath.loans.domain.models.loans.Loan as LoanModel
 import com.apmath.loans.domain.models.payments.Payment as PaymentModel
+import com.apmath.validation.simple.Message
 import io.ktor.application.ApplicationCall
 import io.ktor.response.respond
+import java.lang.NumberFormatException
 
 suspend fun ApplicationCall.v1ListPayments(paymentService: PaymentServiceInterface){
-    val mixed = MixedLoan(
-        getUserId(request),
-        request.headers["service"],
-        parameters["id"]
-    )
+    val isService = isService(request)
+    val loanIdHeader = getLoanId(request)
 
-    val validator = MixedLoanBuilder()
-        .prepend("loanIdHeader", NullableValidator())
-        .prepend("serviceIdHeader", NullableValidator())
-        .prepend("loanId", RequiredValidator())
-        .build()
-
-    if (!validator.validate(mixed)) {
-        respond(validator.messages)
+    val loanId = try {
+        getLoanAttributeId(this)
+    } catch (e: NumberFormatException) {
+        respond(Message("Loan id must be between 1 and ${Int.MAX_VALUE}"))
         return
     }
 
-    val mixedLoanId = mixed.toMixedLoanId()
     val payments =
             try {
-                paymentService.get(mixedLoanId)
-            } catch (e:Exception) {
-                respondError(e)
-                return
+                paymentService.get(isService, loanIdHeader, loanId)
+            } catch (e: LoanNotFoundException) {
+                 NotFoundException("Loan not found")
             }
 
     respond(mapOf("payments" to payments))
+}
+
+private fun getLoanAttributeId(call: ApplicationCall): Int? {
+
+    val headerKey = "loan"
+
+    if (call.parameters.contains(headerKey)) {
+        return call.parameters[headerKey]?.toInt()
+    }
+
+    return null
 }
