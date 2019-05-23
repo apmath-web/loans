a4b4dcb1047b8ceebafa5d015fdec4d067aff25d
meleorina <meleorina@gmail.com>
2019-05-24 01:01:53 +0300

    clean up

--- src/com/apmath/loans/domain/models/MixedLoanId.kt
+++
@@ -1,7 +0,0 @@
-package com.apmath.loans.domain.models
-
-data class MixedLoanId(
-    override val isClient: Boolean,
-    override val loanIdHeader: Int?,
-    override val loanId: Int?
-) : MixedLoanIdInterface
--- src/com/apmath/loans/domain/models/MixedLoanIdInterface.kt
+++
@@ -1,7 +0,0 @@
-package com.apmath.loans.domain.models
-
-interface MixedLoanIdInterface {
-    val isClient: Boolean
-    val loanIdHeader: Int?
-    val loanId: Int?
-}
