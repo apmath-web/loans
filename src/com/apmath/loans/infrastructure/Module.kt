7210bec39eea3f3b48bac1372b4f40e6bedb524c
Neirwin <nightablackstone@gmail.com>
2019-05-23 22:38:22 +0300

    KtorExperimentalAPI

--- src/com/apmath/loans/infrastructure/Module.kt
+++ src/com/apmath/loans/infrastructure/Module.kt
@@ -5,9 +5,11 @@ import com.apmath.loans.domain.repositories.*
 import com.apmath.loans.domain.services.*
 import com.apmath.loans.infrastructure.fetchers.*
 import io.ktor.config.ApplicationConfig
+import io.ktor.util.KtorExperimentalAPI
 import org.koin.dsl.module
 import org.koin.experimental.builder.singleBy
 
+@KtorExperimentalAPI
 /**
  * Module in which services connect to their interfaces
  */
