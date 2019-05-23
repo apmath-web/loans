a84359268400c4d4c36d30a97f237572c6642884
Neirwin <nightablackstone@gmail.com>
2019-05-23 22:07:05 +0300

    Provide config for fetchers with application ENV vars

--- src/com/apmath/loans/infrastructure/Module.kt
+++ src/com/apmath/loans/infrastructure/Module.kt
@@ -4,6 +4,7 @@ import com.apmath.loans.domain.fetchers.*
 import com.apmath.loans.domain.repositories.*
 import com.apmath.loans.domain.services.*
 import com.apmath.loans.infrastructure.fetchers.*
+import io.ktor.config.ApplicationConfig
 import org.koin.dsl.module
 import org.koin.experimental.builder.singleBy
 
@@ -14,9 +15,27 @@ val loans = module {
     //services
     singleBy<LoanServiceInterface, LoanService>()
     //fetchers
-    singleBy<ApplicationsFetcherInterface, ApplicationsFetcher>()
-    singleBy<CalculationsFetcherInterface, CalculationsFetcher>()
-    singleBy<ClientsFetcherInterface, ClientsFetcher>()
+    single {
+        val config = getProperty<ApplicationConfig>("config")
+        ApplicationsFetcher(
+            config.property("applications.host").getString(),
+            config.property("applications.port").getString().toInt()
+        ) as ApplicationsFetcherInterface
+    }
+    single {
+        val config = getProperty<ApplicationConfig>("config")
+        CalculationsFetcher(
+            config.property("calculations.host").getString(),
+            config.property("calculations.port").getString().toInt()
+        ) as CalculationsFetcherInterface
+    }
+    single {
+        val config = getProperty<ApplicationConfig>("config")
+        ClientsFetcher(
+            config.property("clients.host").getString(),
+            config.property("clients.port").getString().toInt()
+        ) as ClientsFetcherInterface
+    }
     //repositories
     singleBy<RepositoryInterface, Repository>()
 }
