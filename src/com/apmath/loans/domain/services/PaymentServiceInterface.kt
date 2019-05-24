937d410949ce2c54ac908bfb1500791f8172df4f
Neirwin <nightablackstone@gmail.com>
2019-05-24 13:04:22 +0300

    deleted unecessary imports in Payment Service interface

--- src/com/apmath/loans/domain/services/PaymentServiceInterface.kt
+++ src/com/apmath/loans/domain/services/PaymentServiceInterface.kt
@@ -1,8 +1,6 @@
 package com.apmath.loans.domain.services
 
-import com.apmath.loans.domain.models.MixedLoanIdInterface
 import com.apmath.loans.domain.models.payments.PaymentFromCalculationInterface
-import com.apmath.loans.domain.models.payments.PaymentInterface
 
 interface PaymentServiceInterface {
     suspend fun get(isService: Boolean, loanIdHeader: Int?, loanId: Int?) : Array<PaymentFromCalculationInterface>
