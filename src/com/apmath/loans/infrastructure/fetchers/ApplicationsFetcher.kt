f7eaffccf90e6f0dd49c24a2dd712cacb75c7ce0
meleorina <meleorina@gmail.com>
2019-05-26 02:17:03 +0300

    formatting

--- src/com/apmath/loans/infrastructure/fetchers/ApplicationsFetcher.kt
+++ src/com/apmath/loans/infrastructure/fetchers/ApplicationsFetcher.kt
@@ -3,7 +3,8 @@ package com.apmath.loans.infrastructure.fetchers
 import com.apmath.loans.domain.fetchers.ApplicationsFetcherInterface
 import com.apmath.loans.infrastructure.models.Application
 
-class ApplicationsFetcher(host: String, port: Int) : AbstractFetcher(host, port), ApplicationsFetcherInterface {
+class ApplicationsFetcher(host: String, port: Int)
+    : AbstractFetcher(host, port), ApplicationsFetcherInterface {
     override suspend fun getApplication(applicationId: Int): Application {
         return get("/v1/$applicationId")
     }
--- src/com/apmath/loans/infrastructure/fetchers/CalculationsFetcher.kt
+++ src/com/apmath/loans/infrastructure/fetchers/CalculationsFetcher.kt
@@ -4,7 +4,8 @@ import com.apmath.loans.domain.fetchers.CalculationsFetcherInterface
 import com.apmath.loans.domain.models.loans.LoanInitializationInterface
 import com.apmath.loans.infrastructure.models.loans.LoanDetails
 
-class CalculationsFetcher(host: String, port: Int) : AbstractFetcher(host, port), CalculationsFetcherInterface {
+class CalculationsFetcher(host: String, port: Int)
+    : AbstractFetcher(host, port), CalculationsFetcherInterface {
     override suspend fun initialization(loan: LoanInitializationInterface): LoanDetails {
         return post("/v1/loan", loan)
     }
