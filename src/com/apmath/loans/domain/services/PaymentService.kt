6e3aea5cee6e61784d02c9e52824353456c295b3
Neirwin <nightablackstone@gmail.com>
2019-05-25 15:53:49 +0300

    deleted unneeded imports in Payment Service

--- src/com/apmath/loans/domain/services/PaymentService.kt
+++ src/com/apmath/loans/domain/services/PaymentService.kt
@@ -1,21 +1,12 @@
 package com.apmath.loans.domain.services
 
-import com.apmath.loans.domain.data.Status
 import com.apmath.loans.domain.data.Type
-import com.apmath.loans.domain.fetchers.ApplicationsFetcherInterface
 import com.apmath.loans.domain.fetchers.CalculationsFetcherInterface
-import com.apmath.loans.domain.fetchers.ClientsFetcherInterface
-import com.apmath.loans.domain.models.MixedId
-import com.apmath.loans.domain.models.MixedIdInterface
-import com.apmath.loans.domain.models.MixedLoanIdInterface
 import com.apmath.loans.domain.models.payments.PaymentInterface
 import com.apmath.loans.domain.models.payments.Payment
 import com.apmath.loans.domain.models.payments.PaymentFromCalculationInterface
 import com.apmath.loans.domain.repositories.RepositoryInterface
 import com.apmath.loans.infrastructure.models.payments.PaymentFromCalculation
-import kotlinx.coroutines.CoroutineStart
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 
 class PaymentService(
     private val calculationsFetcher: CalculationsFetcherInterface,
