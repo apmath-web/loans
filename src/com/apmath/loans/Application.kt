c1c777a575d1ddea891f2531ee44ab32fb437906
BarracudaPff <mrm.kikll@gmail.com>
2019-05-26 00:05:21 +0300

    register Type Adapter for LocalDate

--- src/com/apmath/loans/Application.kt
+++ src/com/apmath/loans/Application.kt
@@ -4,6 +4,9 @@ import com.apmath.loans.application.v1.exceptions.ApiException
 import com.apmath.loans.application.v1.respondApiException
 import com.apmath.loans.application.v1.v1
 import com.apmath.loans.infrastructure.loans
+import com.google.gson.TypeAdapter
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonWriter
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
@@ -15,6 +18,8 @@ import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.routing.Routing
 import org.koin.Logger.slf4jLogger
 import org.koin.ktor.ext.Koin
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -40,6 +45,17 @@ fun Application.module(testing: Boolean = false) {
 
     install(ContentNegotiation) {
         gson {
+            registerTypeAdapter(LocalDate::class.java, object: TypeAdapter<LocalDate>() {
+                val format = DateTimeFormatter.ISO_DATE
+
+                override fun write(out: JsonWriter, value: LocalDate) {
+                    out.value(format.format(value))
+                }
+
+                override fun read(input: JsonReader): LocalDate
+                        = LocalDate.parse(input.nextString(),format)
+
+            })
         }
     }
 
