103ab4883a37469f2d7a77711bc00df52ea6bfc5
meleorina <meleorina@gmail.com>
2019-05-27 00:06:04 +0300

    add fun

--- src/com/apmath/loans/infrastructure/fetchers/CalculationsFetcher.kt
+++ src/com/apmath/loans/infrastructure/fetchers/CalculationsFetcher.kt
@@ -1,6 +1,8 @@
 package com.apmath.loans.infrastructure.fetchers
 
 import com.apmath.loans.domain.fetchers.CalculationsFetcherInterface
+import com.apmath.loans.domain.models.NextCalculationsPaymentInterface
+import com.apmath.loans.domain.models.ResultCalculationsPaymentInterface
 import com.apmath.loans.domain.models.loans.LoanInitializationInterface
 import com.apmath.loans.infrastructure.models.loans.LoanDetails
 
@@ -8,5 +10,13 @@ class CalculationsFetcher(host: String, port: Int)
     : AbstractFetcher(host, port), CalculationsFetcherInterface {
     override suspend fun initialization(loan: LoanInitializationInterface): LoanDetails {
         return post("/v1/loan", loan)
+    }
+
+    override suspend fun nextNewPayment(paymentData: NextCalculationsPaymentInterface): ResultCalculationsPaymentInterface {
+        return post("/v1/payment", paymentData)
+    }
+
+    override suspend fun nextPayment(paymentData: NextCalculationsPaymentInterface): ResultCalculationsPaymentInterface {
+        return put("/v1/payment", paymentData)
     }
 }
