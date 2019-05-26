0c0fe84c0fca992813d0fd4b9d5ea076c4c52b33
meleorina <meleorina@gmail.com>
2019-05-27 00:05:41 +0300

    business logic interface

--- src/com/apmath/loans/domain/services/PaymentServiceInterface.kt
+++ src/com/apmath/loans/domain/services/PaymentServiceInterface.kt
@@ -1,7 +1,8 @@
 package com.apmath.loans.domain.services
 
 import com.apmath.loans.domain.models.payments.PaymentInterface
+import java.time.LocalDate
 
 interface PaymentServiceInterface {
-    suspend fun add(payment: PaymentInterface) : String
+    suspend fun add(payment: PaymentInterface) : LocalDate
 }
