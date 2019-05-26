8ba99ff610bb15731f713234d55a3b42e0b4916c
meleorina <meleorina@gmail.com>
2019-05-26 23:49:52 +0300

    add new fun

--- src/com/apmath/loans/domain/fetchers/CalculationsFetcherInterface.kt
+++ src/com/apmath/loans/domain/fetchers/CalculationsFetcherInterface.kt
@@ -1,8 +1,12 @@
 package com.apmath.loans.domain.fetchers
 
+import com.apmath.loans.domain.models.NextCalculationsPaymentInterface
+import com.apmath.loans.domain.models.ResultCalculationsPaymentInterface
 import com.apmath.loans.domain.models.loans.LoanDetailsInterface
 import com.apmath.loans.domain.models.loans.LoanInitializationInterface
 
 interface CalculationsFetcherInterface {
     suspend fun initialization(loan: LoanInitializationInterface): LoanDetailsInterface
+    suspend fun nextNewPayment(paymentData : NextCalculationsPaymentInterface): ResultCalculationsPaymentInterface
+    suspend fun nextPayment(paymentData : NextCalculationsPaymentInterface): ResultCalculationsPaymentInterface
 }
