f0279da1e8e9047cdb0c3ed14bf1fa9e9bb47cc2
meleorina <meleorina@gmail.com>
2019-05-26 19:33:26 +0300

    fix string to localdate

--- src/com/apmath/loans/domain/services/PaymentService.kt
+++ src/com/apmath/loans/domain/services/PaymentService.kt
@@ -3,14 +3,14 @@ package com.apmath.loans.domain.services
 import com.apmath.loans.domain.fetchers.CalculationsFetcherInterface
 import com.apmath.loans.domain.models.payments.PaymentInterface
 import com.apmath.loans.domain.repositories.RepositoryInterface
+import java.time.LocalDate
 
 class PaymentService(
     private val calculationsFetcher: CalculationsFetcherInterface,
     private val repository: RepositoryInterface
 ) : PaymentServiceInterface {
-    override suspend fun add(payment: PaymentInterface, loanId: Int, clientId: Int): String {
+    override suspend fun add(payment: PaymentInterface, loanId: Int, clientId: Int): LocalDate {
         //TODO("not implemented")
-        return "2019-03-09"
+        return LocalDate.now()
     }
-
 }
