05b5adceedf244cdc0a10324146c484cc4019353
Neirwin <nightablackstone@gmail.com>
2019-05-27 20:37:18 +0300

    fixed inpurt params for some methods

--- src/com/apmath/loans/domain/services/PaymentServiceInterface.kt
+++ src/com/apmath/loans/domain/services/PaymentServiceInterface.kt
@@ -3,5 +3,5 @@ package com.apmath.loans.domain.services
 import com.apmath.loans.domain.models.payments.PaymentFromCalculationInterface
 
 interface PaymentServiceInterface {
-    suspend fun get(loanIdHeader: Int?, loanId: Int?) : Array<PaymentFromCalculationInterface>
+    suspend fun get(loanId: Int, clientId: Int?) : Array<PaymentFromCalculationInterface>
 }
