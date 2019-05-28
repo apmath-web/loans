7132c0a8a9a9b5cde262dd01101d0fe988726e6b
meleorina <meleorina@gmail.com>
2019-05-28 15:13:44 +0300

    for merge

--- src/com/apmath/loans/domain/services/PaymentService.kt
+++ src/com/apmath/loans/domain/services/PaymentService.kt
@@ -2,6 +2,7 @@ package com.apmath.loans.domain.services
 
 import com.apmath.loans.domain.data.Type
 import com.apmath.loans.domain.fetchers.CalculationsFetcherInterface
+import com.apmath.loans.domain.models.payments.PaymentFromCalculationInterface
 import com.apmath.loans.domain.models.payments.PaymentInterface
 import com.apmath.loans.domain.repositories.RepositoryInterface
 import com.apmath.loans.infrastructure.models.payments.PaymentFromCalculation
