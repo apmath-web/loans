a856ba333a140c9b054f48b58ddf090606e7ffd0
meleorina <meleorina@gmail.com>
2019-05-31 11:16:36 +0300

    fix

--- src/com/apmath/loans/domain/models/loans/LoanInterface.kt
+++ src/com/apmath/loans/domain/models/loans/LoanInterface.kt
@@ -8,7 +8,7 @@ interface LoanInterface : LoanCreationDataInterface {
     //also LoanCreationDataInterface here
     var id: Int?
 
-    val interest: Int
+    val interest: Float
     val rounding: Int
     var regularPaymentAmount: Money
     var remainingTerm: Int
