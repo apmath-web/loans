a2f3ff281901b4ef210373775dfb9905d809f9d3
meleorina <meleorina@gmail.com>
2019-06-01 10:37:31 +0300

    add writeof fun

--- src/com/apmath/loans/domain/models/loans/Loan.kt
+++ src/com/apmath/loans/domain/models/loans/Loan.kt
@@ -4,6 +4,8 @@ import com.apmath.loans.domain.data.Currency
 import com.apmath.loans.domain.data.Money
 import com.apmath.loans.domain.data.Type
 import com.apmath.loans.domain.exceptions.runtime.ChangeIdentifiedCreditIdException
+import com.apmath.loans.domain.models.ResultCalculationsPaymentInterface
+import java.time.LocalDate
 import com.apmath.loans.domain.models.payments.PaymentFromCalculationInterface as ResponsePaymentInterface
 
 class Loan(
@@ -42,6 +44,23 @@ class Loan(
             results.addAll(payments.filter { it.type == type })
             results
         }
+    }
+
+    override fun writeOf(resultPayment: ResultCalculationsPaymentInterface) : LocalDate {
+        val loanDetails = resultPayment.loan
+        val payment = resultPayment.payment
+
+        amount -= payment.body
+        remainingTerm--
+        regularPaymentAmount = loanDetails.regularPaymentAmount
+        remainingTerm = loanDetails.remainingTerm
+
+        payments.add(payment)
+
+        if (loanDetails.remainingTerm == 0) {
+            completed = true
+        }
+        return payment.date
     }
 
 }
