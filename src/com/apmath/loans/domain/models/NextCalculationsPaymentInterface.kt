ff1dfdb727dba6b8b39111951d4c1c40bfdaa489
meleorina <meleorina@gmail.com>
2019-06-01 07:23:44 +0300

    create interface

---
+++ src/com/apmath/loans/domain/models/NextCalculationsPaymentInterface.kt
@@ -0,0 +1,9 @@
+import com.apmath.loans.domain.models.loans.LoanForCalculationsInterface
+import com.apmath.loans.domain.models.payments.PaymentDateInterface
+import com.apmath.loans.domain.models.payments.PaymentLastInterface
+
+interface NextCalculationsPaymentInterface {
+    val loan: LoanForCalculationsInterface
+    val lastPayment: PaymentLastInterface?
+    val payment: PaymentDateInterface
+}
