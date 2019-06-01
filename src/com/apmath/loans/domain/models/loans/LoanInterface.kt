21bbfa3c7b710edc017e9aaae72b7d038c32782b
meleorina <meleorina@gmail.com>
2019-06-01 07:24:23 +0300

    fix vals

--- src/com/apmath/loans/domain/models/loans/LoanInterface.kt
+++ src/com/apmath/loans/domain/models/loans/LoanInterface.kt
@@ -2,18 +2,20 @@ package com.apmath.loans.domain.models.loans
 
 import com.apmath.loans.domain.data.Money
 import com.apmath.loans.domain.data.Type
+import com.apmath.loans.domain.models.ResultCalculationsPaymentInterface
+import java.time.LocalDate
 import com.apmath.loans.domain.models.payments.PaymentFromCalculationInterface as ResponsePaymentInterface
 
 interface LoanInterface : LoanCreationDataInterface {
     //also LoanCreationDataInterface here
     var id: Int?
 
-    val interest: Int
+    val interest: Float
     val rounding: Int
-    val regularPaymentAmount: Money
-    val remainingTerm: Int
-    val completed: Boolean
+    var regularPaymentAmount: Money
+    var remainingTerm: Int
+    var completed: Boolean
 
-    fun getPayments(type: Type?): List<ResponsePaymentInterface>
-    fun writeOf(payment: ResponsePaymentInterface)
+    fun getPayments(type: Type? = null): List<ResponsePaymentInterface>
+    fun writeOf(resultPayment: ResultCalculationsPaymentInterface) : LocalDate
 }
